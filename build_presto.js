94dc020057bca492e2b90e97b2e87005e85f9afe
Maurits Lamers <maurits@weidestraat.nl>
2015-02-27 20:57:50 +0100

    add node-js build script for combined presto.js

---
+++ build_presto.js
@@ -0,0 +1,25 @@
+/*jshint node: true*/
+
+var fs = require('fs');
+
+var files = [
+  'license.js',
+  "lib/base.js",
+  "lib/font_info.js",
+  "lib/grob.js",
+  "lib/symbol.js",
+  "lib/line.js",
+  "lib/stem.js",
+  "lib/note.js",
+  "lib/rest.js",
+  "lib/column.js",
+  "lib/note_column.js",
+  "lib/staff.js",
+  "lib/score.js"
+];
+
+var ret = files.map(function (f) {
+  return fs.readFileSync(f).toString();
+});
+
+fs.writeFileSync('presto.js', ret.join("\n"));
